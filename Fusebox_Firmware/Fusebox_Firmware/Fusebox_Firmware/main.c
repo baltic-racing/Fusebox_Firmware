fd0795b787711c605b3fe2b072c82cabdaac9ce3
adam123467 <adaam1157@gmail.com>
2023-02-13 11:42:37 +0100

    Init Project

---
+++ Fusebox_Firmware/Fusebox_Firmware/Fusebox_Firmware/main.c
@@ -0,0 +1,18 @@
+/*
+ * Fusebox_Firmware.c
+ *
+ * Created: 13.02.2023 11:41:48
+ * Author : adaam
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
