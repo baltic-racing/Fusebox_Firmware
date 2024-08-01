74781129e4db275aa8da144de9432f5d6d5efbbe
Marwin-Stk <94782248+AaronWeinstrauch@users.noreply.github.com>
2024-08-01 16:10:41 +0200

    changed baud rate

--- Fusebox_Firmware/Fusebox_Firmware/Fusebox_Firmware/main.c
+++ Fusebox_Firmware/Fusebox_Firmware/Fusebox_Firmware/main.c
@@ -44,7 +44,11 @@ int main(void)
 		{
 			time_10ms = sys_time;
 			
+<<<<<<< Updated upstream
 			AIR_close = ((~PINB & (1 << PB6 ))>> PB6);		// TS_ACT on PB6 
+=======
+			AIR_close = ((~PINB & (1 << PB6 ))>> PB6);		// TS_ACT on PB6
+>>>>>>> Stashed changes
 			
 			PINB = (IMD_LED_light << PB4);					// IMD light on PB4
 
@@ -57,6 +61,7 @@ int main(void)
 			
 			
 			
+<<<<<<< Updated upstream
  			Fusebox0_databytes[0]	=	0						;
 			Fusebox0_databytes[1]	=	0						;	
  			Fusebox0_databytes[2]	=	0						;	
@@ -64,19 +69,34 @@ int main(void)
 			Fusebox0_databytes[4]	=	0						;
 			Fusebox0_databytes[5]	=	AIR_close				;
  			Fusebox0_databytes[6]	=	0						;
+=======
+			Fusebox0_databytes[0]	=	0						;
+			Fusebox0_databytes[1]	=	0						;
+			Fusebox0_databytes[2]	=	0						;
+			Fusebox0_databytes[3]	=	0						;
+			Fusebox0_databytes[4]	=	0						;
+			Fusebox0_databytes[5]	=	AIR_close				;
+			Fusebox0_databytes[6]	=	0						;
+>>>>>>> Stashed changes
 			Fusebox0_databytes[7]	=	0						;
 			
 			
 		
 			
+<<<<<<< Updated upstream
 		
 			 
+=======
+			
+			
+			
+>>>>>>> Stashed changes
 			can_tx(&can_Fusebox0_mob, Fusebox0_databytes);	//(0x600 --> Board Voltages)
 			
 			
 			
 		}	//end of 10 ms cycle
-	
+		
 		if (TIME_PASSED_100_MS)
 		{
 			time_100ms = sys_time;
@@ -84,16 +104,28 @@ int main(void)
 			
 			
 			
+<<<<<<< Updated upstream
 	
  		}  //end of 100ms
 		 
+=======
+			
+		}  //end of 100ms
+		
+>>>>>>> Stashed changes
 		if (TIME_PASSED_200_MS)
 		{
 			time_200ms = sys_time;
 			
+<<<<<<< Updated upstream
 		
 			
 		
+=======
+			
+			
+			
+>>>>>>> Stashed changes
 		} //end of 200ms
 
 	}  //end of while
