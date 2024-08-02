5c0ade056b470d87d225fc622021d0884382b5c5
Marwin-Stk <94782248+AaronWeinstrauch@users.noreply.github.com>
2024-08-02 16:16:28 +0200

    clean up

--- Fusebox_Firmware/Fusebox_Firmware/Handcart FB/main.c
+++ Fusebox_Firmware/Fusebox_Firmware/Handcart FB/main.c
@@ -44,12 +44,11 @@ int main(void)
 		{
 			time_10ms = sys_time;
 			
-<<<<<<< Updated upstream
+
 			AIR_close = ((~PINB & (1 << PB6 ))>> PB6);		// TS_ACT on PB6 
-=======
+
 			AIR_close = ((~PINB & (1 << PB6 ))>> PB6);		// TS_ACT on PB6
->>>>>>> Stashed changes
-			
+
 			PINB = (IMD_LED_light << PB4);					// IMD light on PB4
 
 			can_rx(&can_BMS3_mob, BMS3_databytes);
@@ -57,11 +56,6 @@ int main(void)
 			
 			IMD_LED_light = (BMS3_databytes[6]>>7);
 			
-			
-			
-			
-			
-<<<<<<< Updated upstream
  			Fusebox0_databytes[0]	=	0						;
 			Fusebox0_databytes[1]	=	0						;	
  			Fusebox0_databytes[2]	=	0						;	
@@ -69,31 +63,9 @@ int main(void)
 			Fusebox0_databytes[4]	=	0						;
 			Fusebox0_databytes[5]	=	AIR_close				;
  			Fusebox0_databytes[6]	=	0						;
-=======
-			Fusebox0_databytes[0]	=	0						;
-			Fusebox0_databytes[1]	=	0						;
-			Fusebox0_databytes[2]	=	0						;
-			Fusebox0_databytes[3]	=	0						;
-			Fusebox0_databytes[4]	=	0						;
-			Fusebox0_databytes[5]	=	AIR_close				;
-			Fusebox0_databytes[6]	=	0						;
->>>>>>> Stashed changes
 			Fusebox0_databytes[7]	=	0						;
 			
-			
-		
-			
-<<<<<<< Updated upstream
-		
-			 
-=======
-			
-			
-			
->>>>>>> Stashed changes
 			can_tx(&can_Fusebox0_mob, Fusebox0_databytes);	//(0x600 --> Board Voltages)
-			
-			
 			
 		}	//end of 10 ms cycle
 		
@@ -102,32 +74,7 @@ int main(void)
 			time_100ms = sys_time;
 			sys_tick_heart();
 			
-			
-			
-<<<<<<< Updated upstream
-	
- 		}  //end of 100ms
-		 
-=======
-			
 		}  //end of 100ms
-		
->>>>>>> Stashed changes
-		if (TIME_PASSED_200_MS)
-		{
-			time_200ms = sys_time;
-			
-<<<<<<< Updated upstream
-		
-			
-		
-=======
-			
-			
-			
->>>>>>> Stashed changes
-		} //end of 200ms
-
 	}  //end of while
 } //end of main
 //end of the world
