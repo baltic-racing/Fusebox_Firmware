042d49e40f89fe12aeb5004c70014f59f43a283e
AaronWeinstrauch <94782248+AaronWeinstrauch@users.noreply.github.com>
2024-11-01 18:28:02 +0100

    added adc start conversion
    LVV is now working

--- Fusebox_Firmware/Fusebox_Firmware/Fusebox_Firmware/main.c
+++ Fusebox_Firmware/Fusebox_Firmware/Fusebox_Firmware/main.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
 
+
 extern struct CAN_MOB can_Fusebox0_mob;
 extern struct CAN_MOB can_Fusebox1_mob;
 extern struct CAN_MOB can_Fusebox2_mob;
@@ -40,6 +41,7 @@ uint16_t R2D_counter;
 uint8_t R2D_active;
 uint32_t accufan_counter=0;
 uint8_t Akku_fan_on = 0;
+uint16_t lv_bat = 0;
 
 volatile uint16_t Motor_Temp;
 extern volatile uint16_t fan_dc;
@@ -72,6 +74,7 @@ int main(void)
 		if(TIME_PASSED_1_MS)
 		{
 			time_1ms = sys_time;
+			adc_start_conversion();
 		}
 		
 		if(TIME_PASSED_10_MS)
@@ -86,7 +89,6 @@ int main(void)
 			can_rx(&can_BMS3_mob, BMS3_databytes);
 			can_rx(&can_SWC0_mob, SWC0_databytes);
 			
-			
 			if (DRV_EN==1)
 			{
 				ac_current = calculate_ac_current(current_limit, APPS);
@@ -95,11 +97,14 @@ int main(void)
 			{
 				ac_current = 0;
 			}
-			
+			uint16_t adc1 =adc_get(1);
+			uint16_t lv_bat = (uint16_t)(((adc1 - 17.714) * 0.03712)*10);
+
+
  			Fusebox0_databytes[0]	=	adc_get(0)&0xff			;
 			Fusebox0_databytes[1]	=	(adc_get(0)>>8)&0xff	;	
- 			Fusebox0_databytes[2]	=	adc_get(1)&0xff			;	
- 			Fusebox0_databytes[3]	=	(adc_get(1)>>8)&0xff	; 
+ 			Fusebox0_databytes[2]	=	lv_bat	     &0xff      ;	
+ 			Fusebox0_databytes[3]	=   (lv_bat >>8) &0xff      ; 
 			Fusebox0_databytes[4]	=	0						;
 			Fusebox0_databytes[5]	=	0						;
  			Fusebox0_databytes[6]	=	0						;
