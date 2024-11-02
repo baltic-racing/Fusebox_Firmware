aff6e27b94e8e3240b5baef30c77c341a7658f7a
AaronWeinstrauch <94782248+AaronWeinstrauch@users.noreply.github.com>
2024-11-02 19:08:04 +0100

    Version for Testday
    removed APPSOK, added R2D_bit for ac current calc

--- Fusebox_Firmware/Fusebox_Firmware/Fusebox_Firmware/main.c
+++ Fusebox_Firmware/Fusebox_Firmware/Fusebox_Firmware/main.c
@@ -31,8 +31,6 @@ extern uint8_t SWC0_databytes[8];
 #define AKKUFAN (uint8_t) SWC0_databytes[3]
 #define COOLINGFAN (uint8_t) SWC0_databytes[2]
 
-
-
 uint8_t DRV_EN = 0;
 uint8_t R2D_pressed = 0;
 int16_t ac_current = 0;
@@ -42,6 +40,10 @@ uint8_t R2D_active;
 uint32_t accufan_counter=0;
 uint8_t Akku_fan_on = 0;
 uint16_t lv_bat = 0;
+uint8_t BMS3_databytes[8];
+uint8_t TS_RDY = 0;
+uint8_t R2D_bit = 0;
+uint16_t FRO_Byte = 0;
 
 volatile uint16_t Motor_Temp;
 extern volatile uint16_t fan_dc;
@@ -58,12 +60,6 @@ int main(void)
 	can_cfg();
 	adc_config();
 	CAN_Init_Messages();
-	
-
-	uint8_t BMS3_databytes[8];
-	int8_t TS_RDY = 0;
-	uint8_t R2D_bit = 0;
-	uint16_t FRO_Byte = 0;
 
 	sei();
 
@@ -89,7 +85,7 @@ int main(void)
 			can_rx(&can_BMS3_mob, BMS3_databytes);
 			can_rx(&can_SWC0_mob, SWC0_databytes);
 			
-			if (DRV_EN==1)
+			if (R2D_bit==1)
 			{
 				ac_current = calculate_ac_current(current_limit, APPS);
 			}
@@ -97,10 +93,10 @@ int main(void)
 			{
 				ac_current = 0;
 			}
+			
 			uint16_t adc1 =adc_get(1);
-			uint16_t lv_bat = (uint16_t)(((adc1 - 17.714) * 0.03712)*10);
-
-
+			uint16_t lv_bat = (uint16_t)(((adc1 - 17.714) * 0.03712)*10);  // calculation for LV Battery voltage
+ 
  			Fusebox0_databytes[0]	=	adc_get(0)&0xff			;
 			Fusebox0_databytes[1]	=	(adc_get(0)>>8)&0xff	;	
  			Fusebox0_databytes[2]	=	lv_bat	     &0xff      ;	
@@ -119,18 +115,18 @@ int main(void)
 			//	TS ACTIVATE PROCEDURE
 			if (TSRDY == 1)
 			{
-				if ((TSACT == 1) && (R2D_bit == 0) && (APPSOK == 0))
+				if ((TSACT == 1) && (R2D_bit == 0))// && (APPSOK == 0))
 				{
 					R2D_activation();
 					R2D_bit = 1;
 					DRV_EN = 1;
 				}
-				if (APPSOK==1)
-				{
-					DRV_EN = 0;
-					R2D_bit = 0;
-					
-				}
+				//if (APPSOK==1)
+				//{
+					//DRV_EN = 0;
+					//R2D_bit = 0;
+					//
+				//}
 			}
 			else
 			{
@@ -154,8 +150,6 @@ int main(void)
 					PORTB |= (1<<PB0);
 				}
 				
-				
-			
 				accufan_counter = 0;
 				
 			}
@@ -164,11 +158,11 @@ int main(void)
 				PORTB &= ~(1<<PB6);
 			}
 			
-			Fusebox2_databytes[0] = DRV_EN;
+			//Fusebox2_databytes[0] = DRV_EN;
 			 
 			can_tx(&can_Fusebox0_mob, Fusebox0_databytes);	//(0x600 --> Board Voltages)
 			can_tx(&can_Fusebox1_mob, Fusebox1_databytes);	//(0x601 --> SDC Indicator)
-			can_tx(&can_Fusebox2_mob, Fusebox2_databytes);	//(DRV Enable)
+			//can_tx(&can_Fusebox2_mob, Fusebox2_databytes);	//(DRV Enable)
 			can_tx(&can_Fusebox3_mob, Fusebox3_databytes);	//(AC Current)
 			can_tx(&can_Fusebox4_mob, Fusebox4_databytes);	//(AC Current Limit)
 				
@@ -186,19 +180,19 @@ int main(void)
 		if (TIME_PASSED_200_MS)
 		{
 			time_200ms = sys_time;
-			
-			FRO_Byte = Fuse_Read_Out();
-			
-			
-			Fusebox1_databytes[0]	= 0;
-			Fusebox1_databytes[1]	= 0;
-			Fusebox1_databytes[2]	=	FRO_Byte & 0xFF;
-			Fusebox1_databytes[3]	=	(FRO_Byte >> 8) & 0xFF;
-			Fusebox1_databytes[4]	= 0;
-			Fusebox1_databytes[5]	= 0;
-			Fusebox1_databytes[6]	= 0;
-			Fusebox1_databytes[7]	= 0;
-			
+			//
+			//FRO_Byte = Fuse_Read_Out();
+			//
+			//
+			//Fusebox1_databytes[0]	= 0;
+			//Fusebox1_databytes[1]	= 0;
+			//Fusebox1_databytes[2]	=	FRO_Byte & 0xFF;
+			//Fusebox1_databytes[3]	=	(FRO_Byte >> 8) & 0xFF;
+			//Fusebox1_databytes[4]	= 0;
+			//Fusebox1_databytes[5]	= 0;
+			//Fusebox1_databytes[6]	= 0;
+			//Fusebox1_databytes[7]	= 0;
+			//
 			
 		} //end of 200ms
 
